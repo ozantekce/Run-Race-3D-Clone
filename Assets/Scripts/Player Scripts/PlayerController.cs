2a7a1c435e679e2243434527b3bdf623e2e7580d
ozantekce <32203867+ozantekce@users.noreply.github.com>
2022-08-02 23:08:44 +0300

    Update 3

--- Assets/Scripts/Player Scripts/PlayerController.cs
+++ Assets/Scripts/Player Scripts/PlayerController.cs
@@ -8,6 +8,9 @@ public class PlayerController : MonoBehaviour
     private Vector3 move;
     public float speed, jumpForce, gravity, verticalVelocity;
 
+    private bool wallSlide;
+
+    private bool doubleJump;
     private CharacterController charController;
 
 
@@ -24,22 +27,66 @@ public class PlayerController : MonoBehaviour
 
         if (charController.isGrounded)
         {
+            wallSlide = false;
             verticalVelocity = 0;
             if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
             {
                 verticalVelocity = jumpForce;
+                doubleJump = true;
             }
         }
-        else
+        else if ((Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) && doubleJump)
+        {
+            verticalVelocity += jumpForce * 0.5f;
+            doubleJump = false;
+        }
+
+        if (!wallSlide)
         {
             gravity = 30;
             verticalVelocity -= gravity * Time.deltaTime;
         }
+        else
+        {
+            gravity = 15;
+            verticalVelocity -= gravity * Time.deltaTime;
+        }
+
 
         move.Normalize();
         move *= speed;
         move.y = verticalVelocity;
         charController.Move(move * Time.deltaTime);
+        print(wallSlide);
+
+    }
+
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+
+        if (!charController.isGrounded)
+        {
+            if(hit.collider.tag == "Wall")
+            {
+                if(verticalVelocity < -0.6f)
+                    wallSlide = true;
+                if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+                {
+                    verticalVelocity = jumpForce;
+
+                    doubleJump = false;
+
+                    transform.eulerAngles = new Vector3(
+                        transform.eulerAngles.x
+                        , transform.eulerAngles.y + 180
+                        , transform.eulerAngles.z);
+
+                    wallSlide = false;
+                }
+            }
+
+        }
 
     }
 
